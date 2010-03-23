173dbf5c08c4cfea489779f8cf6f9b50a2df04c6
unknown <Dobi Attila@.(none)>
2010-03-23 19:35:28 +0100

    skeleton

---
+++ jquery.editable.js
@@ -0,0 +1,13 @@
+(function($) {
+    
+    $.fn.editable = function(options) {
+        
+        options = $.extend({}, $.fn.editable.defaults, options);
+    };
+    
+    $.fn.editable.defaults = {
+        
+    }
+    
+    
+}) (jQuery);
